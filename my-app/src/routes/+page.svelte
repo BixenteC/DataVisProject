2982b2204f206419d6f7c9242b557dc80e88fd8f
Bixente Cornelis <bixente.cornelis@student.kuleuven.be>
2023-04-26 18:48:33 +0200

    Added interactivity on the overview page.

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
 	import { scaleLinear, scaleOrdinal } from 'd3-scale';
 	import { schemeDark2 } from 'd3-scale-chromatic';
+	import { createEventDispatcher } from 'svelte';
 	export let data;
+
+	const dispatch = createEventDispatcher();
 
 	// Dimensions
 	const width = 600;
@@ -20,15 +23,50 @@
 	// Color scale
 	const locationTypesUniques = [...new Set(data.pointsOfInterest.map((v) => v.type))];
 	const ordinalScale = scaleOrdinal(schemeDark2).domain(locationTypesUniques);
+
+	// Drop down selection
+	const unique_car_ids = [...new Set(data.GPSTracking.map((v) => v.car_id))];
+	let selected_car_id = -1; // Default value
+
+	function selectCar() {
+		dispatch('message', {
+			value: selected_car_id
+		});
+	}
 </script>
 
-<h1>Bixente Cornelis - KU Leuven - r0716823</h1>
-<h2>Overview</h2>
+<head>
+	<title>Data Visualisation Project</title>
+</head>
+
+<body>
+	<h1>Bixente Cornelis - KU Leuven - r0716823</h1>
+	<h2>Overview</h2>
+
+	<label for="dropDownCar">Select car to highlight:</label>
+	<select bind:value={selected_car_id} on:change={selectCar}>
+		<option value={-1}>-- Select car --</option>
+		{#each unique_car_ids as id}
+			<option value={id}>Car {id}</option>
+		{/each}
+	</select><br />
+	{#if selected_car_id !== -1}
+		Go to <a href="/overview">details</a> for car {selected_car_id}.
+	{/if}
+</body>
 
 <svg {width} {height}>
+	<!-- GPS Tracking Data -->
 	{#each data.GPSTracking as dp}
-		<circle cx={longScale(dp.long)} cy={height - latScale(dp.lat)} r="3" />
+		<circle
+			cx={longScale(dp.long)}
+			cy={height - latScale(dp.lat)}
+			r="3"
+			class:selectedCarGPS={dp.car_id == selected_car_id}
+		/>
 	{/each}
+
+	<!-- Points of interest -->
 	{#each data.pointsOfInterest as dp}
 		<circle
 			cx={longScale(dp.long)}
@@ -36,7 +74,8 @@
 			r="5"
 			fill={ordinalScale(dp.type)}
 			class="locationType"
-		/>
+			><title>{dp.name}</title>
+		</circle>
 	{/each}
 </svg>
 
@@ -46,5 +85,9 @@
 	}
 	circle.locationType {
 		fill-opacity: 1;
+	}
+	circle.selectedCarGPS {
+		fill-opacity: 0.8;
+		fill: red;
 	}
 </style>
